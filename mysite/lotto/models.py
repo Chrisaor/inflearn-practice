6167ec07837535f38f5c7f4f1cd897ea08b94870
chrisaor <pchpch0070@gmail.com>
2018-02-26 22:47:50 +0900

    url 수정 및  lotto.models 구현

--- mysite/lotto/models.py
+++ mysite/lotto/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import random
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class GuessNumbers(models.Model):
+    name = models.CharField(max_length=24)
+    lottos = models.CharField(
+        max_length=255,
+        default='[1,2,3,4,5,6]',
+    )
+    text = models.CharField(max_length=255)
+    num_lottos = models.IntegerField(default = 5)
+    update_date = models.DateTimeField()
+
+    def generate(self):
+        self.lottos = ""
+        origin = list(range(1,46))
+        for _ in range(0, self.num_lottos):
+            random.shuffle(origin)
+            guess = origin[:6]
+            guess.sort()
+            self.lottos += str(guess) + '\n'
+        self.update_date = timezone.now()
+        self.save()
